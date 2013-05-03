68b1e18df0e4460139e898393c29d61f70c2eb6c
Ari Perez <ariperez83@gmail.com>
2013-05-03 15:27:00 -0400

    Push Fix to make Map Work

--- app/models/googlemap.rb
+++ app/models/googlemap.rb
@@ -6,7 +6,7 @@ acts_as_gmappable
 
 def gmaps4rails_address
 #describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki
-  self.address
+  "#{self.address}" 
 end
 
 
