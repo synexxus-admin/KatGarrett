dcdc4f9e4dc133b2607096655b4ac59d1a47fc6e
Synexxus Admin <synexxus.portal@gmail.com>
2013-05-03 20:05:21 -0400

    nothing

--- app/controllers/googlemaps_controller.rb
+++ app/controllers/googlemaps_controller.rb
@@ -3,8 +3,8 @@ class GooglemapsController < ApplicationController
   # GET /googlemaps.json
   def index
     @googlemaps = Googlemap.all
-
     @json = Googlemap.all.to_a.to_gmaps4rails
+
 
 
     respond_to do |format|
